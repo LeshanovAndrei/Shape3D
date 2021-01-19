18ba6cfc95d2ba4b6e2b693e3017fc91eebbced7
b.dan.18.09@gmail.com <b.dan.18.09@gmail.com>
2021-01-19 15:20:27 +0200

    Prism and Sphere signature

--- Shape3D/Shape3D_app/Shapes/Prism.h
+++ Shape3D/Shape3D_app/Shapes/Prism.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Shape.h"
+class Prism:public Shape
+{
+public:
+	Prism();
+	~Prism();
+	void Draw() override;
+private:
+	float* vertices;
+	UINT VBO;
+	UINT VAO;
+};
+
+Prism::Prism()
+{
+}
+
+Prism::~Prism()
+{
+}
--- Shape3D/Shape3D_app/Shapes/Sphere.h
+++ Shape3D/Shape3D_app/Shapes/Sphere.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Shape.h"
+class Sphere:public Shape
+{
+public:
+	Sphere();
+	~Sphere();
+	void Draw() override;
+private:
+	float* vertices;
+	UINT VBO;
+	UINT VAO;
+};
+
+Sphere::Sphere()
+{
+}
+
+Sphere::~Sphere()
+{
+}
