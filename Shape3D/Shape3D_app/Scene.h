c96bbf1643657f7fefada50e8f2da31108798667
LeshanovAndrei <leshanov.andrei@gmail.com>
2021-01-19 15:47:15 +0200

    Scene signature added

--- Shape3D/Shape3D_app/Scene.h
+++ Shape3D/Shape3D_app/Scene.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "includes.h"
+#include "Controls/Controller.h"
+#include "Camera.h"
+
+class Scene
+{
+public:
+	Scene();
+	~Scene();
+	void RedrawEverything();
+	void EventsCheck();
+
+private:
+	std::unique_ptr<Controller> mainControl;
+	std::unique_ptr<Camera> mainCamera;
+};
+
+Scene::Scene()
+{
+}
+
+Scene::~Scene()
+{
+}
