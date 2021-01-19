c2e8e9a2ef924b7460f696eda736d32703da1124
b.dan.18.09@gmail.com <b.dan.18.09@gmail.com>
2021-01-19 15:35:46 +0200

    Controller and Controller1 signature

--- Shape3D/Shape3D_app/Controls/Controller.h
+++ Shape3D/Shape3D_app/Controls/Controller.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "../includes.h"
+class Controller
+{
+public:
+	Controller();
+	~Controller();
+	virtual void Move() = 0;
+	virtual void ChangeSize(float) = 0;
+	virtual void ChangeColor() = 0;
+	virtual void DrawObjects() = 0;
+	virtual void NextObject() = 0;
+	virtual void PrewObject() = 0;
+	virtual void Aggregate() = 0;
+	virtual void Undo() = 0;
+private:
+
+};
+
+Controller::Controller()
+{
+}
+
+Controller::~Controller()
+{
+}
